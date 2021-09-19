1599f4f08e0ea72705b7f9b51cd214fcc63aeadc
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2021-09-19 17:51:31 +0100

    use renderRoutes & BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router } from 'react-router'
-
-import App from './App'
-import history from './history'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
 
 import './index.css'
-import reportWebVitals from './reportWebVitals'
+import routes from './routes'
+
+// import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router history={history}>
-      <App />
+    <Router>
+      {renderRoutes(routes)}
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
@@ -20,4 +20,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log)
+// reportWebVitals(console.log)
