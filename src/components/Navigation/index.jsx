5334394eb4355d08a2a7bb4ac2536a55e3fcc626
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2021-09-21 18:28:57 +0100

    add logo to header

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom'
 import Link from './Link'
+import logo from '@src/assets/svg/logo.svg'
 
 const Navigation = () => {
 
@@ -11,6 +12,7 @@ const Navigation = () => {
           to="/"
           className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0'
         >
+          <img src={logo} alt="" />
           <span className='cursor-pointer ml-3 text-xl'>Code Adventures</span>
         </NavLink>
         <nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center'>
