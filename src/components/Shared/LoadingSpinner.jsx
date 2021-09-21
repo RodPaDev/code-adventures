abae4ad0ddfe4ddf4670e1dee6af5ab8b3ac0455
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2021-09-21 18:02:55 +0100

    add loading spinner

---
+++ src/components/Shared/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+const LoadingSpinner = () => {
+  return (
+    <div className='absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 '>
+      <div
+        style={{ borderTopColor: 'transparent' }}
+        className='border-solid animate-spin  rounded-full border-yellow-500 border-8 h-64 w-64'
+      ></div>
+    </div>
+  )
+}
+
+export default LoadingSpinner
