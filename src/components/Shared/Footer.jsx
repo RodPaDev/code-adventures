3c668d8cd8c565795285c23ddd38aeae33e1d144
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2021-09-21 17:42:16 +0100

    create Footer component

---
+++ src/components/Shared/Footer.jsx
@@ -0,0 +1,44 @@
+import githubSvg from 'src/assets/svg/github.svg'
+import linkedinSvg from 'src/assets/svg/linkedin.svg'
+import twitterSvg from 'src/assets/svg/twitter.svg'
+import SocialLink from './SocialLink'
+
+const Footer = () => {
+  return (
+    <footer className='text-gray-600 body-font'>
+      <div className='container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col'>
+        <p className='text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4'>
+          Made by
+          <a
+            href='https://twitter.com/knyttneve'
+            className='text-gray-600 ml-1'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            Patrick Rodrigues
+          </a>{' '}
+          {'and powered by 🚀 fuel.'}
+        </p>
+        <span className='inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start'>
+          <SocialLink
+            src={linkedinSvg}
+            ariaLabel='LinkedIn'
+            link='https://www.linkedin.com/in/rodpadev/'
+          />
+          <SocialLink
+            src={twitterSvg}
+            ariaLabel='Twitter'
+            link='https://twitter.com/RodPaDev'
+          />
+          <SocialLink
+            src={githubSvg}
+            ariaLabel='GitHub'
+            link='https://github.com/RodPaDev'
+          />
+        </span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
